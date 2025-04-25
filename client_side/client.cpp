73ac6af29b3bf87bbee17a481add8a4f21df505e
ales2026 <ale.urankar@gmail.com>
2025-04-25 05:57:16 +0200

    receive unique id from serfver

--- client_side/client.cpp
+++ client_side/client.cpp
@@ -3,15 +3,19 @@
 
 int main() {
     using namespace boost::asio;
-    io_context io;                 // Main I/O context for asynchronous operations
+    io_context io;                 // 1.Main I/O context for asynchronous operations
 
-    ip::tcp::socket socket(io);    // Create a socket for the client
-    socket.connect(ip::tcp::endpoint(ip::make_address("127.0.0.1"), 1234)); // Connect to the server
+    ip::tcp::socket socket(io);    // 1.Create a socket for the client
+    socket.connect(ip::tcp::endpoint(ip::make_address("127.0.0.1"), 1234)); // 1.Connect to the server
 
-    std::string msg = "Hello Server!";      // Message to send to the server
-    write(socket, buffer(msg));             // Send the message
+    char welcome[128];       // 2.Receive welcome message from server
+    size_t id_len = socket.read_some(buffer(welcome));
+    std::cout << std::string(welcome, id_len); // 2.Print assigned client ID
 
-    char reply[128];                                              // Buffer for the server's reply
-    size_t len = socket.read_some(buffer(reply));                 // Read server response
-    std::cout << "Reply: " << std::string(reply, len) << "\n";    // Print the response
+    std::string msg = "Hello From Client!";      // 1.Message to send to the server
+    write(socket, buffer(msg));             // 1.Send the message
+
+    char reply[128];                                              // 1.Buffer for the server's reply
+    size_t len = socket.read_some(buffer(reply));                 // 1.Read server response
+    std::cout << "Server Replyed: " << std::string(reply, len) << "\n";    // 1.Print the response
 }
