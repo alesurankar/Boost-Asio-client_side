3ac2bfd8433b757cf53fe97eaff27db0af248693
ales2026 <ale.urankar@gmail.com>
2025-11-29 10:11:40 +0100

    adding and linking boost

--- client_side/Main.cpp
+++ client_side/Main.cpp
@@ -1,14 +1,17 @@
 ﻿#include "ChatClient.h"
 #include <iostream>
-#include <fstream>
+#include <fstream> 
+#include <string>
+#include <filesystem>
 
 int main() {
-    std::ifstream file("C:/Projects/Python_API_setup_for_NinjaStrike/username.txt");
+    std::ifstream file("username.txt");
     std::string username;
-    if (file.is_open()) 
+    if (file.is_open())
     {
         std::getline(file, username);
     }
+
     boost::asio::io_context io;
     ChatClient client(io, "127.0.0.1", 1234, username);
     client.Start();
